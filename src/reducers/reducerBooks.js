9c11dadb687886fd523b3031e96481a62fff5a89
Cody <hoban.j.cody@gmail.com>
2020-12-07 20:36:40 -0500

    creating book list

---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The GOod Parts', pages: 101 },
+        { title: 'Harry Otter', pages: 10 },
+        { title: 'The Dark Tower', pages: 121 },
+        { title: 'Eloquent Ruby', pages: 11 }
+    ]
+}
